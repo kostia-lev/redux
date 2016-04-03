25f431e31d5e7dff6f1b837b54e6de3c59bfe52c
Konstantin <vahkos@mail.ru>
2016-04-03 15:56:17 -0400

    tutorial finished

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import User from '../components/User'
+import Page from '../components/Page'
+import * as pageActions from '../actions/PageActions'
+import * as userActions from '../actions/UserActions'
 
-export default class App extends Component {
+class App extends Component {
     render() {
-        return <div>Привет из App, show me not babe, just </div>
+        const { user, page } = this.props
+        const { getPhotos } = this.props.pageActions
+        const { handleLogin } = this.props.userActions
+
+        return <div>
+            <User name={user.name} />
+            <Page photos={page.photos} year={page.year} getPhotos={getPhotos} fetching={page.fetching}/>
+            <User name={user.name} handleLogin={handleLogin} error={user.error} />
+        </div>
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        user: state.user,
+        page: state.page
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        pageActions: bindActionCreators(pageActions, dispatch),
+        userActions: bindActionCreators(userActions, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path')
 var webpack = require('webpack')
 var NpmInstallPlugin = require('npm-install-webpack-plugin')
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -17,10 +19,10 @@ module.exports = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new NpmInstallPlugin() // <--
+    new NpmInstallPlugin()
   ],
-  module: { //Обновлено
-    preLoaders: [ //добавили ESlint в preloaders
+  module: {
+    preLoaders: [
       {
         test: /\.js$/,
         loaders: ['eslint'],
@@ -29,15 +31,22 @@ module.exports = {
         ],
       }
     ],
-    loaders: [ //добавили babel-loader
+    loaders: [
       {
-        loaders: ['react-hot','babel-loader'],
+        loaders: ['react-hot', 'babel-loader'],
         include: [
           path.resolve(__dirname, "src"),
         ],
         test: /\.js$/,
         plugins: ['transform-runtime'],
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
   }
 }
